8c5da1135625c4a2659aeff8bb7697fd6d6a9094
Jose Luis Represa <joseluis.represa@bbva.com>
2018-03-07 15:01:16 +0100

    fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,14 @@
 module.exports = {
-  root: true,
-  parserOptions: {
-    ecmaVersion: 2017,
-    sourceType: 'module'
-  },
-  extends: 'eslint-config-bbva',
-  env: {
-    browser: true
-  }
+	root: true,
+	parserOptions: {
+		ecmaVersion: 2017,
+		sourceType: 'module'
+	},
+	extends: 'eslint-config-bbva',
+	env: {
+		node: true
+	},
+	rules: {
+		'no-sync': 1
+	}
 };
--- testem.js
+++ testem.js
@@ -1,15 +1,15 @@
 module.exports = {
-  framework: 'qunit',
-  test_page: 'tests/index.html?hidepassed',
-  disable_watching: true,
-  browser_start_timeout: 180,
-  launch_in_ci: [
-    'Chrome'
-  ],
-  launch_in_dev: [
-    'Chrome'
-  ],
-  browser_args: {
+	framework: 'qunit',
+	test_page: 'tests/index.html?hidepassed',
+	disable_watching: true,
+	browser_start_timeout: 180,
+	launch_in_ci: [
+		'Chrome'
+	],
+	launch_in_dev: [
+		'Chrome'
+	],
+	browser_args: {
 		Chrome: [
 			'--headless',
 			'--disable-gpu',
